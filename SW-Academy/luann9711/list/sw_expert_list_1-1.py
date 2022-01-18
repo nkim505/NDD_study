8299af1b0cc3fbf4d2b817f3acf6a933a54dc59b
luann9711 <marchen9708@gmail.com>
2022-01-18 09:34:33 +0900

    <코드>: 1일차 - min max
    문제 : list - 1일차 - min max
    N개의 양의 정수에서 가장 큰 수와 가장 작은 수의 차이를 출력하시오.
    
    ----------------------------------------------------
    수정 사항
    
     -  한번의 테스트케이스 종료 후 그 케이스에 대한 결과값을 바로 출력하는 것이 아닌
        마지막에 각 테스트케이스에 대한 결과값을  한번에 출력하도록 수정
    
     -  sorted로 정렬해서 최대값 최빈값을 찾아서 연산했던 로직을 max(), min() 함수를 이용하여 개선함
    
     -  counting 을 따로 주지 않고 enumerate() 를 이용하여 자동 인덱싱이 되도록 개선함
    
     -  입력단에 안내메시지를 추가하고 범위밖의 숫자가 입력되었을때는 재입력 할수 있게 개선

--- SW-Academy/luann9711/list/sw_expert_list_1-1.py
+++ SW-Academy/luann9711/list/sw_expert_list_1-1.py
@@ -1,22 +1,41 @@
 # 테스트 케이스 입력받기
-test_case = int(input())
-# 인덱스 숫자 카운팅
-counting = 1
+test_case = int(input('테스트 케이스 1이상 50 이하의 숫자를 입력해 주세요 : '))
+
+# 요구범위 밖의 숫자가 입력되었을때 오류메시지 출력 후 재 입력
+while (test_case > 50) or (test_case <= 0):
+    if (test_case > 50) or (test_case <= 0):
+        print('범위 밖의 숫자입니다 다시 입력해주세요')
+        test_case = int(input('테스트 케이스 1이상 50 이하의 숫자를 입력해 주세요 : '))
+
+# 결과값을 저장하는 리스트
 answer = []
 
+# 테스트 케이스가 0보다 큰 상황에서 반복시키고 0이 되면 종료시킨다
 while test_case > 0:
-    num_split = int(input())
-    num = input().split()
+    # 양수의 갯수 N 을 입력 받는다.
+    n = int(input('양수의 갯수를 5이상 1000이하로 입력해주세요 : '))
+    
+    # 요구범위 밖의 숫자가 입력되었을때 오류메시지 출력 후 재 입력
+    while (n > 1000) or (n < 5):
+        if (n > 1000) or (n < 5):
+            print('범위 밖의 숫자입니다 다시 입력해주세요')
+            n = int(input('양수의 갯수를 5이상 1000이하로 입력해주세요 : '))
 
+    # N 개의 양수를 입력받는다
+    num = input('{}개의 양수를 입력해주세요 : '.format(n)).split()
+    
+    # num 으로 받은 리스트의 양수들을 숫자형으로 변환시킨다.
     for idx in range(len(num)) :
         num[idx] = int(num[idx])
-
-    num = sorted(num)
-    calc = num[-1] - num[0]
-
-    print('#{}'.format(counting), calc)
-
+    
+    # 리스트에 있는 최고 값에서 최저 값을 빼서 변수 calc에 저장한다 - max(), min() 함수 사용
+    calc = max(num) - min(num)
+    # calc 의 결과 값을 외부 list answer에 추가해 준다
+    answer.append(calc)
+    
+    # 남은 테스트 케이스 숫자를 1 감소시킨다
     test_case -= 1
-    counting += 1
-
-
+    
+# answer list 에 저장된 요소들은 enumerate() 함수로 인덱스를 붙여 차례대로 출력해준다
+for idx, ans in enumerate(answer, start=1):
+    print('#',idx, ans)
