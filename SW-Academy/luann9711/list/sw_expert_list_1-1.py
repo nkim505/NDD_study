d3fec0c695e2c7af974476ae5694d6de212e16d7
luann9711 <marchen9708@gmail.com>
2022-01-22 03:14:36 +0900

    <코드>: 1일차 - min max
    문제 : list - 1일차 - min max
    N개의 양의 정수에서 가장 큰 수와 가장 작은 수의 차이를 출력하시오.
    
    ----------------------------------------------------
    수정 사항
    
     -  한번의 테스트케이스 종료 후 그 케이스에 대한 결과값을 바로 출력하여 메모리 성능을 개선
    
     -  입력단에 안내메시지를 추가하고 범위밖의 숫자가 입력되었을때는 재입력 할수 있게 개선

--- SW-Academy/luann9711/list/sw_expert_list_1-1.py
+++ SW-Academy/luann9711/list/sw_expert_list_1-1.py
@@ -1,26 +1,26 @@
-# 테스트 케이스 입력받기
+import time
+
+# 테스트 케이스를 입력 받는다.
 test_case = int(input('테스트 케이스 1이상 50 이하의 숫자를 입력해 주세요 : '))
 
-# 요구범위 밖의 숫자가 입력되었을때 오류메시지 출력 후 재 입력
+# 요구범위 밖의 숫자가 입력되었을때 오류메시지 출력 후 재입력
 while (test_case > 50) or (test_case <= 0):
-    if (test_case > 50) or (test_case <= 0):
-        print('범위 밖의 숫자입니다 다시 입력해주세요')
-        test_case = int(input('테스트 케이스 1이상 50 이하의 숫자를 입력해 주세요 : '))
+    print('범위 밖의 숫자입니다 다시 입력해주세요')
+    test_case = int(input('테스트 케이스 1이상 50 이하의 숫자를 입력해 주세요 : '))
 
-# 결과값을 저장하는 리스트
-answer = []
+# 출력시 인덱스에 사용할 카운트 넘버
+count = 1
 
-# 테스트 케이스가 0보다 큰 상황에서 반복시키고 0이 되면 종료시킨다
+# 프로그램 전체를 입력된 케이스 수 만큼 반복시킨다
 while test_case > 0:
     # 양수의 갯수 N 을 입력 받는다.
     n = int(input('양수의 갯수를 5이상 1000이하로 입력해주세요 : '))
     
     # 요구범위 밖의 숫자가 입력되었을때 오류메시지 출력 후 재 입력
     while (n > 1000) or (n < 5):
-        if (n > 1000) or (n < 5):
-            print('범위 밖의 숫자입니다 다시 입력해주세요')
-            n = int(input('양수의 갯수를 5이상 1000이하로 입력해주세요 : '))
-
+        print('범위 밖의 숫자입니다 다시 입력해주세요')
+        n = int(input('양수의 갯수를 5이상 1000이하로 입력해주세요 : '))
+    
     # N 개의 양수를 입력받는다
     num = input('{}개의 양수를 입력해주세요 : '.format(n)).split()
     
@@ -29,13 +29,25 @@ while test_case > 0:
         num[idx] = int(num[idx])
     
     # 리스트에 있는 최고 값에서 최저 값을 빼서 변수 calc에 저장한다 - max(), min() 함수 사용
+
+    # time 모듈을 사용하여 실행속도 측정
+    # sorted = O(n log n)
+    # min(), max() = O(n)
+
+    start_time = time.time()
+    # num = sorted(num)
+    # calc = num[-1] - num[0]
     calc = max(num) - min(num)
-    # calc 의 결과 값을 외부 list answer에 추가해 준다
-    answer.append(calc)
+    end_time = time.time()
+
+    print('max - min 실행시간 {} 초'.format(end_time - start_time))
+    
+
+    # calc 의 결과 값을 인덱스와 함께 정해진 형식으로 출력한다.
+    print(f'#{count} {calc}')
     
     # 남은 테스트 케이스 숫자를 1 감소시킨다
+    # 남은 테스트 케이스가 0이 되면 while 문에 의해 자동으로 종료됨
     test_case -= 1
-    
-# answer list 에 저장된 요소들은 enumerate() 함수로 인덱스를 붙여 차례대로 출력해준다
-for idx, ans in enumerate(answer, start=1):
-    print('#',idx, ans)
+    # 인덱스에 사용할 카운트 넘버를 상승시킨다
+    count += 1
