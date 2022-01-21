f08f2a3777b7481e14977e53874b109d79a26898
boron-nadaedae <boron.nadaedae@gmail.com>
2022-01-21 11:55:11 -0600

    <Fix> L2Q5 update exceptions, change to f-string

--- SW-Academy/boron/02_List/list02_q5.py
+++ SW-Academy/boron/02_List/list02_q5.py
@@ -1,19 +1,26 @@
 # List2-5th-count_purple
-print('Type test case number (T), 1<=T<=50')
-T = int(input())
-if (T < 1) or (T > 50):
-    raise ValueError('T should be a positive integer, 1<=T<=50')
+try:
+    T = int(input('Type test case number (T) between 1 and 50'))
+except ValueError:
+    print('Type positive integer')
+if not (1 <= T <= 50):
+    raise ValueError('T should be 1 <= T <= 50')
 for test_case in range(1, T + 1):
-    print('Type number of colored area (N), N is a integer and 2<=N<=30')
-    N = int(input())
-    if (N < 2) or (N > 30):
+    try:
+        N = int(input('Type number of colored area (N), 2<=N<=30'))
+    except ValueError:
+        print('ValueError: type only an integer!')
+    if not (2 <= N <= 30):
         raise ValueError('N should be an integer between 2 and 30')
     colored_area_idx = []
     colored_area_color = []
     for num_area in range(N):
-        print('Type'+str(num_area)+'th area left corner index (r1,c1), \
-            right corner index (r2,c2),  and color (1 or 2)')
-        r1, c1, r2, c2, pcolor = map(int, input().split())
+        try:
+            r1, c1, r2, c2, pcolor = map(int, input(f'Type{num_area}th area \
+                left corner index (r1,c1),right corner index (r2,c2),\
+                and color (1 or 2)').split())
+        except ValueError:
+            print('Type only five integers separated by white space')
         if not all(0 <= corner_idx <= 9 for corner_idx in [r1, c1, r2, c2]):
             raise ValueError('r1, c1, r2, c2 should be between 0 and 9')
         elif (r1 <= r2) or (c1 <= c2):
@@ -35,4 +42,4 @@ for test_case in range(1, T + 1):
         if colored_area_color[count] == 3:
             purple = purple+1
     print("Output: [#Test number] [Number of purple area]")
-    print("#"+str(test_case)+" "+str(purple))
+    print(f"#{test_case} {purple}")
