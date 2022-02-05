99db8a0f95ccfae2bd0e2aa45b9918d7b7712df6
luann9711 <marchen9708@gmail.com>
2022-02-05 19:16:04 +0900

    <리팩토링> : Stack - 그래프 경로
     - 기능 일부를 함수(캡슐)화 하여 반복문 밖으로 빼두고 반복문 안에서는 함수만 실행

--- SW-Academy/luann9711/stack/stack_1-3.py
+++ SW-Academy/luann9711/stack/stack_1-3.py
@@ -18,6 +18,18 @@ def push(stack, n):
 def pop(stack):
     return stack.pop()
 
+
+def dfs(x):
+        visted[x] = True
+        push(stack, x)
+
+        for y in way_list[x]:
+            if visted[y] == False:
+                dfs(y)
+            
+            if y == b:
+                return True
+
 while t > 0:
     try:
         v, e = map(int, input('노드 수와 간선 수를 입력 : ').split())
@@ -32,17 +44,6 @@ while t > 0:
     visted = [False for _ in range(v + 1)]
     way_list = [[] for _ in range(v + 1)]
 
-    def dfs(x):
-        visted[x] = True
-        push(stack, x)
-
-        for y in way_list[x]:
-            if visted[y] == False:
-                dfs(y)
-            
-            if y == b:
-                return True
-    
     # e 의 갯수 만큼 출발노드와 도착노드의 간선정보를 입력받는다.
     for _ in range(e):
         a, b = map(int, input('출발노드와 도착노드의 간선정보를 입력 : ').split())
@@ -53,9 +54,12 @@ while t > 0:
     for e in way_list:
         e.sort()
     
+
     # 존재를 확인 할 출발노드와 도착노드의 간선정보를 입력받는다.
     s, g = map(int, input('존재를 확인할 출발노드와 도착노드의 루트를 입력 : ').split())
     
+
+    # 함수 dfs 를 실행하고 참값을떄 1을 아닐떄는 0를 출력한다
     if dfs(a) == True:
         print(f'#{c - t} {1}')
     else:
